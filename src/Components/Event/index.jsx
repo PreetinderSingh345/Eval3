d8f18e4b52bd01bddaffc8b778fe774395a9b3f7
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-03-01 13:08:58 +0530

    feat: add basic styling to header, footer and filterbox

--- src/Components/Event/index.jsx
+++ src/Components/Event/index.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './Event.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 function Event() {
   return (
     <div className="event">
       <img src="" alt="" className="event-img" />
-      <span className="event-name">Name</span>
+      <span className="event-name">NAME</span>
       <p className="event-description">
         This is some sample description This is some sample description This is
         some sample description This is some sample description
       </p>
-      <span className="venue">Venue</span>
-      <span className="date-time">DateTime</span>
-      <div className="registered-bookmarked">
-        <span>Registered</span>
-        <span>Bookmarked</span>
+      <span className="event-venue">VENUE:</span>
+      <span className="event-date-time">DATE:</span>
+      <div className="event-registered-bookmarked">
+        <span className="event-registered">
+          <FontAwesomeIcon icon={faCheckCircle} className="event-icons" />
+          <span>REGISTERED</span>
+        </span>
+        <FontAwesomeIcon
+          icon={faBookmark}
+          className="event-bookmarked event-icons"
+        />
       </div>
     </div>
   );
