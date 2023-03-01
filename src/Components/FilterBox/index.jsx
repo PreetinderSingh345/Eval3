21de8c61c52cd0e0f973b5f3897e38340f5df5e5
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-03-01 16:36:34 +0530

    feat: make basic structure of filter options

--- src/Components/FilterBox/index.jsx
+++ src/Components/FilterBox/index.jsx
@@ -1,29 +1,73 @@
 import React from 'react';
+import { useState } from 'react';
 import './FilterBox.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faFilter,
   faAngleUp,
+  faAngleDown,
   faMagnifyingGlass,
 } from '@fortawesome/free-solid-svg-icons';
 
-function FilterBox() {
+function FilterBox({ filter, setFilter }) {
+  function handleFilterToggle() {
+    setFilter(!filter);
+  }
+
   return (
-    <div id="filter-box">
-      <div id="filter">
-        <FontAwesomeIcon icon={faFilter} className="filter-box-icons" />
-        <span>FILTER</span>
-        <FontAwesomeIcon icon={faAngleUp} className="filter-box-icons" />
+    <>
+      <div id="filter-box">
+        <div id="filter">
+          <FontAwesomeIcon icon={faFilter} className="filter-box-icons" />
+          <span>FILTER</span>
+          {filter ? (
+            <FontAwesomeIcon
+              icon={faAngleUp}
+              className="filter-box-icons"
+              onClick={handleFilterToggle}
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faAngleDown}
+              className="filter-box-icons"
+              onClick={handleFilterToggle}
+            />
+          )}
+        </div>
+
+        <div id="search">
+          <input type="text" placeholder="EVENT NAME" id="search-input" />
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="filter-box-icons"
+          />
+        </div>
       </div>
 
-      <div id="search">
-        <input type="text" placeholder="EVENT NAME" id="search-input" />
-        <FontAwesomeIcon
-          icon={faMagnifyingGlass}
-          className="filter-box-icons"
-        />
-      </div>
-    </div>
+      {filter && (
+        <form id="filter-options">
+          <div className="filter-radio-buttons">
+            <label for="all">ALL</label>
+            <input type="radio" id="all" name="options" />
+          </div>
+
+          <div className="filter-radio-buttons">
+            <label for="bookmarked">BOOKMARKED</label>
+            <input type="radio" id="bookmarked" name="options" />
+          </div>
+
+          <div className="filter-radio-buttons">
+            <label for="registered">REGISTERED</label>
+            <input type="radio" id="registered" name="options" />
+          </div>
+
+          <div className="filter-radio-buttons">
+            <label for="seats-available">SEATS AVAILABLE</label>
+            <input type="radio" id="seats-available" name="options" />
+          </div>
+        </form>
+      )}
+    </>
   );
 }
 
--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 import './Main.css';
 import FilterBox from '../FilterBox';
 import Events from '../Events';
 
 function Main() {
+  const [filter, setFilter] = useState(false);
+
   return (
     <main>
-      <FilterBox />
+      <FilterBox filter={filter} setFilter={setFilter} />
       <Events />
     </main>
   );
